f466eea4e8c4c05aff7de755d6dc8011a5a0fd10
smjsportscardsnc <smjsportscardsnc@gmail.com>
2026-05-04 14:43:58 -0400

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,28 +1,25 @@
 // SMJ Firebase config
-// Paste your existing Firebase web app config below.
-// This overlay uses Firebase Realtime Database.
+// Firebase Realtime Database control bridge for SMJ overlay.
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyAP5JIYtql-4YB-wAgNT2Urja4DUOFoRws",
+  authDomain: "smj-overlay.firebaseapp.com",
+  databaseURL: "https://smj-overlay-default-rtdb.firebaseio.com",
+  projectId: "smj-overlay",
+  storageBucket: "smj-overlay.firebasestorage.app",
+  messagingSenderId: "1034121319902",
+  appId: "1:1034121319902:web:2b88ce1cc3ccf79766794f",
+  measurementId: "G-511F87E4GE"
 };
 
 window.SMJFIREBASE_READY = false;
 window.SMJFIREBASE_ERROR = null;
 
 try {
-  const hasPlaceholder = Object.values(firebaseConfig).some(v => String(v).includes("PASTE_"));
-  if (hasPlaceholder) {
-    throw new Error("Firebase config still has placeholder values in js/firebase.js");
-  }
   firebase.initializeApp(firebaseConfig);
   window.smjDB = firebase.database();
   window.SMJFIREBASE_READY = true;
+  console.log("SMJ Firebase connected");
 } catch (err) {
   console.error("SMJ Firebase setup error:", err);
   window.SMJFIREBASE_ERROR = err.message || String(err);
