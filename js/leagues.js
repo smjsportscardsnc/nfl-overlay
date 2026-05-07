e28b1c862e1466112456e52056e88e1c533d24f0
smjsportscardsnc <smjsportscardsnc@gmail.com>
2026-05-07 15:17:19 -0400

    Add files via upload

---
+++ js/leagues.js
@@ -0,0 +1,854 @@
+window.SMJ_LEAGUES = {
+  "nfl": {
+    "name": "NFL",
+    "cdn": "nfl",
+    "teams": [
+      {
+        "abbr": "ari",
+        "city": "Arizona",
+        "name": "Cardinals",
+        "color": "#97233F"
+      },
+      {
+        "abbr": "atl",
+        "city": "Atlanta",
+        "name": "Falcons",
+        "color": "#A71930"
+      },
+      {
+        "abbr": "bal",
+        "city": "Baltimore",
+        "name": "Ravens",
+        "color": "#241773"
+      },
+      {
+        "abbr": "buf",
+        "city": "Buffalo",
+        "name": "Bills",
+        "color": "#00338D"
+      },
+      {
+        "abbr": "car",
+        "city": "Carolina",
+        "name": "Panthers",
+        "color": "#0085CA"
+      },
+      {
+        "abbr": "chi",
+        "city": "Chicago",
+        "name": "Bears",
+        "color": "#0B162A"
+      },
+      {
+        "abbr": "cin",
+        "city": "Cincinnati",
+        "name": "Bengals",
+        "color": "#FB4F14"
+      },
+      {
+        "abbr": "cle",
+        "city": "Cleveland",
+        "name": "Browns",
+        "color": "#311D00"
+      },
+      {
+        "abbr": "dal",
+        "city": "Dallas",
+        "name": "Cowboys",
+        "color": "#003594"
+      },
+      {
+        "abbr": "den",
+        "city": "Denver",
+        "name": "Broncos",
+        "color": "#FB4F14"
+      },
+      {
+        "abbr": "det",
+        "city": "Detroit",
+        "name": "Lions",
+        "color": "#0076B6"
+      },
+      {
+        "abbr": "gb",
+        "city": "Green Bay",
+        "name": "Packers",
+        "color": "#203731"
+      },
+      {
+        "abbr": "hou",
+        "city": "Houston",
+        "name": "Texans",
+        "color": "#03202F"
+      },
+      {
+        "abbr": "ind",
+        "city": "Indianapolis",
+        "name": "Colts",
+        "color": "#002C5F"
+      },
+      {
+        "abbr": "jax",
+        "city": "Jacksonville",
+        "name": "Jaguars",
+        "color": "#006778"
+      },
+      {
+        "abbr": "kc",
+        "city": "Kansas City",
+        "name": "Chiefs",
+        "color": "#E31837"
+      },
+      {
+        "abbr": "lv",
+        "city": "Las Vegas",
+        "name": "Raiders",
+        "color": "#A5ACAF"
+      },
+      {
+        "abbr": "lac",
+        "city": "Los Angeles",
+        "name": "Chargers",
+        "color": "#0080C6"
+      },
+      {
+        "abbr": "lar",
+        "city": "Los Angeles",
+        "name": "Rams",
+        "color": "#003594"
+      },
+      {
+        "abbr": "mia",
+        "city": "Miami",
+        "name": "Dolphins",
+        "color": "#008E97"
+      },
+      {
+        "abbr": "min",
+        "city": "Minnesota",
+        "name": "Vikings",
+        "color": "#4F2683"
+      },
+      {
+        "abbr": "ne",
+        "city": "New England",
+        "name": "Patriots",
+        "color": "#002244"
+      },
+      {
+        "abbr": "no",
+        "city": "New Orleans",
+        "name": "Saints",
+        "color": "#D3BC8D"
+      },
+      {
+        "abbr": "nyg",
+        "city": "New York",
+        "name": "Giants",
+        "color": "#0B2265"
+      },
+      {
+        "abbr": "nyj",
+        "city": "New York",
+        "name": "Jets",
+        "color": "#125740"
+      },
+      {
+        "abbr": "phi",
+        "city": "Philadelphia",
+        "name": "Eagles",
+        "color": "#004C54"
+      },
+      {
+        "abbr": "pit",
+        "city": "Pittsburgh",
+        "name": "Steelers",
+        "color": "#FFB612"
+      },
+      {
+        "abbr": "sf",
+        "city": "San Francisco",
+        "name": "49ers",
+        "color": "#AA0000"
+      },
+      {
+        "abbr": "sea",
+        "city": "Seattle",
+        "name": "Seahawks",
+        "color": "#002244"
+      },
+      {
+        "abbr": "tb",
+        "city": "Tampa Bay",
+        "name": "Buccaneers",
+        "color": "#D50A0A"
+      },
+      {
+        "abbr": "ten",
+        "city": "Tennessee",
+        "name": "Titans",
+        "color": "#4B92DB"
+      },
+      {
+        "abbr": "wsh",
+        "city": "Washington",
+        "name": "Commanders",
+        "color": "#5A1414"
+      }
+    ]
+  },
+  "nba": {
+    "name": "NBA",
+    "cdn": "nba",
+    "teams": [
+      {
+        "abbr": "atl",
+        "city": "Atlanta",
+        "name": "Hawks",
+        "color": "#E03A3E"
+      },
+      {
+        "abbr": "bos",
+        "city": "Boston",
+        "name": "Celtics",
+        "color": "#007A33"
+      },
+      {
+        "abbr": "bkn",
+        "city": "Brooklyn",
+        "name": "Nets",
+        "color": "#000"
+      },
+      {
+        "abbr": "cha",
+        "city": "Charlotte",
+        "name": "Hornets",
+        "color": "#1D1160"
+      },
+      {
+        "abbr": "chi",
+        "city": "Chicago",
+        "name": "Bulls",
+        "color": "#CE1141"
+      },
+      {
+        "abbr": "cle",
+        "city": "Cleveland",
+        "name": "Cavaliers",
+        "color": "#860038"
+      },
+      {
+        "abbr": "dal",
+        "city": "Dallas",
+        "name": "Mavericks",
+        "color": "#00538C"
+      },
+      {
+        "abbr": "den",
+        "city": "Denver",
+        "name": "Nuggets",
+        "color": "#0E2240"
+      },
+      {
+        "abbr": "det",
+        "city": "Detroit",
+        "name": "Pistons",
+        "color": "#C8102E"
+      },
+      {
+        "abbr": "gsw",
+        "city": "Golden State",
+        "name": "Warriors",
+        "color": "#1D428A"
+      },
+      {
+        "abbr": "hou",
+        "city": "Houston",
+        "name": "Rockets",
+        "color": "#CE1141"
+      },
+      {
+        "abbr": "ind",
+        "city": "Indiana",
+        "name": "Pacers",
+        "color": "#002D62"
+      },
+      {
+        "abbr": "lac",
+        "city": "LA",
+        "name": "Clippers",
+        "color": "#C8102E"
+      },
+      {
+        "abbr": "lal",
+        "city": "Los Angeles",
+        "name": "Lakers",
+        "color": "#552583"
+      },
+      {
+        "abbr": "mem",
+        "city": "Memphis",
+        "name": "Grizzlies",
+        "color": "#5D76A9"
+      },
+      {
+        "abbr": "mia",
+        "city": "Miami",
+        "name": "Heat",
+        "color": "#98002E"
+      },
+      {
+        "abbr": "mil",
+        "city": "Milwaukee",
+        "name": "Bucks",
+        "color": "#00471B"
+      },
+      {
+        "abbr": "min",
+        "city": "Minnesota",
+        "name": "Timberwolves",
+        "color": "#0C2340"
+      },
+      {
+        "abbr": "no",
+        "city": "New Orleans",
+        "name": "Pelicans",
+        "color": "#0C2340"
+      },
+      {
+        "abbr": "ny",
+        "city": "New York",
+        "name": "Knicks",
+        "color": "#006BB6"
+      },
+      {
+        "abbr": "okc",
+        "city": "Oklahoma City",
+        "name": "Thunder",
+        "color": "#007AC1"
+      },
+      {
+        "abbr": "orl",
+        "city": "Orlando",
+        "name": "Magic",
+        "color": "#0077C0"
+      },
+      {
+        "abbr": "phi",
+        "city": "Philadelphia",
+        "name": "76ers",
+        "color": "#006BB6"
+      },
+      {
+        "abbr": "phx",
+        "city": "Phoenix",
+        "name": "Suns",
+        "color": "#1D1160"
+      },
+      {
+        "abbr": "por",
+        "city": "Portland",
+        "name": "Trail Blazers",
+        "color": "#E03A3E"
+      },
+      {
+        "abbr": "sac",
+        "city": "Sacramento",
+        "name": "Kings",
+        "color": "#5A2D81"
+      },
+      {
+        "abbr": "sa",
+        "city": "San Antonio",
+        "name": "Spurs",
+        "color": "#C4CED4"
+      },
+      {
+        "abbr": "tor",
+        "city": "Toronto",
+        "name": "Raptors",
+        "color": "#CE1141"
+      },
+      {
+        "abbr": "utah",
+        "city": "Utah",
+        "name": "Jazz",
+        "color": "#002B5C"
+      },
+      {
+        "abbr": "wsh",
+        "city": "Washington",
+        "name": "Wizards",
+        "color": "#002B5C"
+      }
+    ]
+  },
+  "mlb": {
+    "name": "MLB",
+    "cdn": "mlb",
+    "teams": [
+      {
+        "abbr": "ari",
+        "city": "Arizona",
+        "name": "Diamondbacks",
+        "color": "#A71930"
+      },
+      {
+        "abbr": "atl",
+        "city": "Atlanta",
+        "name": "Braves",
+        "color": "#CE1141"
+      },
+      {
+        "abbr": "bal",
+        "city": "Baltimore",
+        "name": "Orioles",
+        "color": "#DF4601"
+      },
+      {
+        "abbr": "bos",
+        "city": "Boston",
+        "name": "Red Sox",
+        "color": "#BD3039"
+      },
+      {
+        "abbr": "chc",
+        "city": "Chicago",
+        "name": "Cubs",
+        "color": "#0E3386"
+      },
+      {
+        "abbr": "chw",
+        "city": "Chicago",
+        "name": "White Sox",
+        "color": "#27251F"
+      },
+      {
+        "abbr": "cin",
+        "city": "Cincinnati",
+        "name": "Reds",
+        "color": "#C6011F"
+      },
+      {
+        "abbr": "cle",
+        "city": "Cleveland",
+        "name": "Guardians",
+        "color": "#00385D"
+      },
+      {
+        "abbr": "col",
+        "city": "Colorado",
+        "name": "Rockies",
+        "color": "#33006F"
+      },
+      {
+        "abbr": "det",
+        "city": "Detroit",
+        "name": "Tigers",
+        "color": "#0C2340"
+      },
+      {
+        "abbr": "hou",
+        "city": "Houston",
+        "name": "Astros",
+        "color": "#002D62"
+      },
+      {
+        "abbr": "kc",
+        "city": "Kansas City",
+        "name": "Royals",
+        "color": "#004687"
+      },
+      {
+        "abbr": "laa",
+        "city": "Los Angeles",
+        "name": "Angels",
+        "color": "#BA0021"
+      },
+      {
+        "abbr": "lad",
+        "city": "Los Angeles",
+        "name": "Dodgers",
+        "color": "#005A9C"
+      },
+      {
+        "abbr": "mia",
+        "city": "Miami",
+        "name": "Marlins",
+        "color": "#00A3E0"
+      },
+      {
+        "abbr": "mil",
+        "city": "Milwaukee",
+        "name": "Brewers",
+        "color": "#12284B"
+      },
+      {
+        "abbr": "min",
+        "city": "Minnesota",
+        "name": "Twins",
+        "color": "#002B5C"
+      },
+      {
+        "abbr": "nym",
+        "city": "New York",
+        "name": "Mets",
+        "color": "#002D72"
+      },
+      {
+        "abbr": "nyy",
+        "city": "New York",
+        "name": "Yankees",
+        "color": "#003087"
+      },
+      {
+        "abbr": "ath",
+        "city": "Athletics",
+        "name": "Athletics",
+        "color": "#003831"
+      },
+      {
+        "abbr": "phi",
+        "city": "Philadelphia",
+        "name": "Phillies",
+        "color": "#E81828"
+      },
+      {
+        "abbr": "pit",
+        "city": "Pittsburgh",
+        "name": "Pirates",
+        "color": "#FDB827"
+      },
+      {
+        "abbr": "sd",
+        "city": "San Diego",
+        "name": "Padres",
+        "color": "#2F241D"
+      },
+      {
+        "abbr": "sf",
+        "city": "San Francisco",
+        "name": "Giants",
+        "color": "#FD5A1E"
+      },
+      {
+        "abbr": "sea",
+        "city": "Seattle",
+        "name": "Mariners",
+        "color": "#0C2C56"
+      },
+      {
+        "abbr": "stl",
+        "city": "St. Louis",
+        "name": "Cardinals",
+        "color": "#C41E3A"
+      },
+      {
+        "abbr": "tb",
+        "city": "Tampa Bay",
+        "name": "Rays",
+        "color": "#092C5C"
+      },
+      {
+        "abbr": "tex",
+        "city": "Texas",
+        "name": "Rangers",
+        "color": "#003278"
+      },
+      {
+        "abbr": "tor",
+        "city": "Toronto",
+        "name": "Blue Jays",
+        "color": "#134A8E"
+      },
+      {
+        "abbr": "wsh",
+        "city": "Washington",
+        "name": "Nationals",
+        "color": "#AB0003"
+      }
+    ]
+  },
+  "nhl": {
+    "name": "NHL",
+    "cdn": "nhl",
+    "teams": [
+      {
+        "abbr": "ana",
+        "city": "Anaheim",
+        "name": "Ducks",
+        "color": "#F47A38"
+      },
+      {
+        "abbr": "bos",
+        "city": "Boston",
+        "name": "Bruins",
+        "color": "#FFB81C"
+      },
+      {
+        "abbr": "buf",
+        "city": "Buffalo",
+        "name": "Sabres",
+        "color": "#002654"
+      },
+      {
+        "abbr": "car",
+        "city": "Carolina",
+        "name": "Hurricanes",
+        "color": "#CC0000"
+      },
+      {
+        "abbr": "cbj",
+        "city": "Columbus",
+        "name": "Blue Jackets",
+        "color": "#002654"
+      },
+      {
+        "abbr": "cgy",
+        "city": "Calgary",
+        "name": "Flames",
+        "color": "#C8102E"
+      },
+      {
+        "abbr": "chi",
+        "city": "Chicago",
+        "name": "Blackhawks",
+        "color": "#CF0A2C"
+      },
+      {
+        "abbr": "col",
+        "city": "Colorado",
+        "name": "Avalanche",
+        "color": "#6F263D"
+      },
+      {
+        "abbr": "dal",
+        "city": "Dallas",
+        "name": "Stars",
+        "color": "#006847"
+      },
+      {
+        "abbr": "det",
+        "city": "Detroit",
+        "name": "Red Wings",
+        "color": "#CE1126"
+      },
+      {
+        "abbr": "edm",
+        "city": "Edmonton",
+        "name": "Oilers",
+        "color": "#041E42"
+      },
+      {
+        "abbr": "fla",
+        "city": "Florida",
+        "name": "Panthers",
+        "color": "#041E42"
+      },
+      {
+        "abbr": "lak",
+        "city": "Los Angeles",
+        "name": "Kings",
+        "color": "#111"
+      },
+      {
+        "abbr": "min",
+        "city": "Minnesota",
+        "name": "Wild",
+        "color": "#154734"
+      },
+      {
+        "abbr": "mtl",
+        "city": "Montreal",
+        "name": "Canadiens",
+        "color": "#AF1E2D"
+      },
+      {
+        "abbr": "njd",
+        "city": "New Jersey",
+        "name": "Devils",
+        "color": "#CE1126"
+      },
+      {
+        "abbr": "nsh",
+        "city": "Nashville",
+        "name": "Predators",
+        "color": "#FFB81C"
+      },
+      {
+        "abbr": "nyi",
+        "city": "New York",
+        "name": "Islanders",
+        "color": "#00539B"
+      },
+      {
+        "abbr": "nyr",
+        "city": "New York",
+        "name": "Rangers",
+        "color": "#0038A8"
+      },
+      {
+        "abbr": "ott",
+        "city": "Ottawa",
+        "name": "Senators",
+        "color": "#C52032"
+      },
+      {
+        "abbr": "phi",
+        "city": "Philadelphia",
+        "name": "Flyers",
+        "color": "#F74902"
+      },
+      {
+        "abbr": "pit",
+        "city": "Pittsburgh",
+        "name": "Penguins",
+        "color": "#FCB514"
+      },
+      {
+        "abbr": "sea",
+        "city": "Seattle",
+        "name": "Kraken",
+        "color": "#001628"
+      },
+      {
+        "abbr": "sjs",
+        "city": "San Jose",
+        "name": "Sharks",
+        "color": "#006D75"
+      },
+      {
+        "abbr": "stl",
+        "city": "St. Louis",
+        "name": "Blues",
+        "color": "#002F87"
+      },
+      {
+        "abbr": "tbl",
+        "city": "Tampa Bay",
+        "name": "Lightning",
+        "color": "#002868"
+      },
+      {
+        "abbr": "tor",
+        "city": "Toronto",
+        "name": "Maple Leafs",
+        "color": "#00205B"
+      },
+      {
+        "abbr": "uta",
+        "city": "Utah",
+        "name": "Mammoth",
+        "color": "#6CACE4"
+      },
+      {
+        "abbr": "van",
+        "city": "Vancouver",
+        "name": "Canucks",
+        "color": "#00205B"
+      },
+      {
+        "abbr": "vgk",
+        "city": "Vegas",
+        "name": "Golden Knights",
+        "color": "#B4975A"
+      },
+      {
+        "abbr": "wpg",
+        "city": "Winnipeg",
+        "name": "Jets",
+        "color": "#041E42"
+      },
+      {
+        "abbr": "wsh",
+        "city": "Washington",
+        "name": "Capitals",
+        "color": "#041E42"
+      }
+    ]
+  },
+  "wnba": {
+    "name": "WNBA",
+    "cdn": "wnba",
+    "teams": [
+      {
+        "abbr": "atl",
+        "city": "Atlanta",
+        "name": "Dream",
+        "color": "#C8102E"
+      },
+      {
+        "abbr": "chi",
+        "city": "Chicago",
+        "name": "Sky",
+        "color": "#5091CD"
+      },
+      {
+        "abbr": "con",
+        "city": "Connecticut",
+        "name": "Sun",
+        "color": "#F05023"
+      },
+      {
+        "abbr": "dal",
+        "city": "Dallas",
+        "name": "Wings",
+        "color": "#002B5C"
+      },
+      {
+        "abbr": "gs",
+        "city": "Golden State",
+        "name": "Valkyries",
+        "color": "#B9975B"
+      },
+      {
+        "abbr": "ind",
+        "city": "Indiana",
+        "name": "Fever",
+        "color": "#C8102E"
+      },
+      {
+        "abbr": "lv",
+        "city": "Las Vegas",
+        "name": "Aces",
+        "color": "#000"
+      },
+      {
+        "abbr": "la",
+        "city": "Los Angeles",
+        "name": "Sparks",
+        "color": "#552583"
+      },
+      {
+        "abbr": "min",
+        "city": "Minnesota",
+        "name": "Lynx",
+        "color": "#236192"
+      },
+      {
+        "abbr": "ny",
+        "city": "New York",
+        "name": "Liberty",
+        "color": "#86CEBC"
+      },
+      {
+        "abbr": "phx",
+        "city": "Phoenix",
+        "name": "Mercury",
+        "color": "#201747"
+      },
+      {
+        "abbr": "sea",
+        "city": "Seattle",
+        "name": "Storm",
+        "color": "#2C5234"
+      },
+      {
+        "abbr": "wsh",
+        "city": "Washington",
+        "name": "Mystics",
+        "color": "#002B5C"
+      }
+    ]
+  }
+};
